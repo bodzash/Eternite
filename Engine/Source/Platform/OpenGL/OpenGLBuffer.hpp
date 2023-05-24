c5f5322352a47f596f856284c0e3d1d6f5bd7451
Laszlo Bozzay <bozzaylaszlo4@gmail.com>
2023-05-24 13:03:55 +0200

    Added buffer layout stuff

--- Engine/Source/Platform/OpenGL/OpenGLBuffer.hpp
+++ Engine/Source/Platform/OpenGL/OpenGLBuffer.hpp
@@ -12,8 +12,12 @@ namespace Apex
 
 		virtual void Bind() const;
 		virtual void Unbind() const;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -24,6 +28,9 @@ namespace Apex
 
 		virtual void Bind() const;
 		virtual void Unbind() const;
+
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
